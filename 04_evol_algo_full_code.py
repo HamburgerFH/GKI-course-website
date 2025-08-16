2dd7bd4deb8c78cf9108c9717ed5b191a75a1334
IlyaZar <zarubin@wiso.uni-koeln.de>
2025-08-16 18:30:55 +0200

    feat: update lecture 4

--- 04_evol_algo_full_code.py
+++ 04_evol_algo_full_code.py
@@ -7,14 +7,14 @@ from sklearn.model_selection import train_test_split
 from statistics import mean
 def main():
     (popsize, x, y, x_test, y_test, generationen,best, score, score_n, Nachkommen) = Initialisierung()
-    population = anfangspopulation(popsize)
+    population = Anfangspopulation(popsize)
     score = fitness(population, popsize, x, y, score)
     start = mean(fitness(population, popsize, x_test, y_test, score))
     for gen in range(generationen):
         roul = roulette(score)
         for j in range(0, popsize, 2):
-            elter_1, elter_2 = selektion(popsize, population, roul)
-            Nachkommen[j], Nachkommen[j + 1] = rekombination(elter_1, elter_2)
+            Elter_1, Elter_2 = selektion(popsize, population, roul)
+            Nachkommen[j], Nachkommen[j + 1] = rekombination(Elter_1, Elter_2)
         Nachkommen = mutation(Nachkommen, popsize)
         score_n = fitness(Nachkommen, popsize, x, y, score_n)
         population += Nachkommen
@@ -46,19 +46,44 @@ def Anfangspopulation(popsize):
         individuum = [crit,max,min]
         population.append(individuum)
     return population
-
-
-
-
-
-
-
-
-
-
-
-
-
+def fitness(population, popsize, X, y, score):
+    for i in range(0,popsize):
+        clf = tree.DecisionTreeClassifier(criterion=population[i][0], max_depth=population[i][1], min_samples_split=population[i][2])
+        score[i] = mean(cross_val_score(clf,X,y,cv=3))
+    return score
+def roulette(score):
+  score = score / sum(score)
+  roul = np.cumsum(score)
+  return roul
+def elterselektion(roul,popsize):
+    zufallszahl = random.random()
+    Elter = 0
+    for i in range(0,popsize):
+      if (zufallszahl >= roul[i]):
+        Elter = i + 1
+    return Elter
+def selektion(popsize,population,roul):
+    Elter1 = population[elterselektion(roul,popsize)]
+    Elter2 = population[elterselektion(roul,popsize)]
+    return Elter1, Elter2
+def rekombination(Elter1, Elter2):
+    crossover = int(round(random.random()+1))
+    Kind1 = Elter1[0:crossover] + Elter2[crossover:]
+    Kind2 = Elter2[0:crossover] + Elter1[crossover:]
+    return Kind1, Kind2
+def mutation(population,popsize):
+    Mutationswahrscheinlichkeit = 0.2
+    for i in range(0,popsize):
+        if random.random() < Mutationswahrscheinlichkeit:
+            Allel = int(round(random.random()*2))
+            if Allel == 0:
+                c = ['gini','entropy','log_loss']
+                crit = int(round(random.random()*2))
+                population[i][Allel] = c[crit]
+            elif Allel == 1:
+                population[i][Allel] = int(round(random.random()*9+1))
+            elif Allel == 2: population[i][Allel] = int(round(random.random()*9+2))
+    return population
 
 
 
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-1. Ergänze Studienbrief 4., zweiter Teil 3.3 bis 3.6
+1. Ergänze Studienbrief 5 (komplett)
 
-2. Ergänze Studienbrief 5 (komplett)
+2. Update der Notebooks mit mehr comments und Text
 
-3. Update der Notebooks mit mehr comments und Text
-
-4. Füge ein Notebook zur Hyperparameter optimierung hinzu
+3. Füge ein Notebook zur Hyperparameter optimierung hinzu
